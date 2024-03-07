a4a424913e41b8f9e39da7d0dd553bf8aa733524
hyperaid <abhinavsharma2k19@gmail.com>
2024-03-08 02:50:07 +0530

    fixed cors

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app=express();
 
 
+
 const userRoutes=require('./routes/User');
 const profileRoutes=require('./routes/Profile');
 const paymentRoutes=require('./routes/Payments');
@@ -22,10 +23,7 @@ database.connect();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin:"http://localhost:3000",
-    credentials:true
-}));
+app.use(cors());
 
 app.use(fileUpload({
     useTempFiles:true,
